5cc3f5a9104c17822522a2623e4359be96278ea7
LAB-INTERFACES <lab-interfaces@lab-interfacess-macbook-pro.local>
2016-02-09 15:02:22 -0600

    Initial Commit

---
+++ PhotoFilters/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoFilters
+//
+//  Created by LAB-INTERFACES on 2/9/16.
+//  Copyright © 2016 LAB-INTERFACES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
